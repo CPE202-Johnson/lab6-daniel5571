0d4cb76e058878fb8eb2d159243cc23ff7c7cb0f
Daniel5571 <dvernola@calpoly.edu>
2021-02-19 13:20:40 -0800

    lab 6

--- sorts.py
+++ sorts.py
@@ -2,25 +2,47 @@ import random
 import time
 
 def selection_sort(list):
-    pass
-    
+    comparisons = 0                    #not sure if this is correct
+    for i in range(len(list)):
+        minPosition = i
+        for j in range(i+1, len(list)):
+            comparisons +=1
+            if list[minPosition] > list[j]:
+                minPosition = j
+        list[i], list[minPosition] = list[minPosition], list[i]
+    return comparisons
+
+
+# print(selection_sort([23, 10]))
+
 def insertion_sort(list):
-    pass
+    comparisons = 0                      #not sure if this is correct
+    for i in range(1, len(list)):
+        current = list[i]
+        while i > 0 and list[i-1] > current:
+            comparisons+=1
+            list[i] = list[i-1]
+            i = i-1
+            list[i] = current
+    return comparisons
+
+# print(insertion_sort([9, 3, 5, 6, 1, 4]))
+
    
 
 def main():
     # Code coverage NOT required for main
-    # Give the random number generator a seed, so the same sequence of 
+    # Give the random number generator a seed, so the same sequence of
     # random numbers is generated at each run
-    random.seed(1234) 
-    
+    random.seed(1234)
+
     # Generate 5000 random numbers from 0 to 999,999
     randoms = random.sample(range(1000000), 5000)
-    start_time = time.time() 
+    start_time = time.time()
     comps = selection_sort(randoms)
     stop_time = time.time()
     print(comps, stop_time - start_time)
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
 
